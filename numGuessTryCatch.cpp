cee06c92fff0078b112a3c8a5df8f86a8ed0b6c5
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-10-25 17:01:21 +0000

    Ran cpplint.

--- numGuessTryCatch.cpp
+++ numGuessTryCatch.cpp
@@ -7,13 +7,13 @@
 // a guess and my program will tell them
 // if they got it right or wrong.
 // My program will also catch any
-//invalid inputs with a Try Catch.
+// invalid inputs with a Try Catch.
 #include <cstdlib>
 #include <iostream>
 
 int main() {
     // Declaring variables for user guess
-    // as a string and the random number 
+    // as a string and the random number
     // that will be generated.
     std::string userGuessAsString;
     int randomInteger, userGuessAsInteger;
@@ -29,12 +29,14 @@ int main() {
     std::cin >> userGuessAsString;
 
     // Initiating try catch.
-    try { 
+    try {
         userGuessAsInteger = std::stoi(userGuessAsString);
         if (userGuessAsInteger == randomInteger) {
-            std::cout << "You guessed correctly! The number was " << randomInteger;
+            std::cout << "You guessed correctly! The number was "
+            << randomInteger;
         } else {
-            std::cout << "You guessed incorrectly.The correct number was " << randomInteger;
+            std::cout << "You guessed incorrectly.The correct number was "
+            << randomInteger;
             }
     } catch (std::invalid_argument) {
         std::cout
